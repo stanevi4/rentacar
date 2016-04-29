31ae340953b9296ac3c13b9b2ac95ff2b8af6e7f
serg <sstbox@gmail.com>
2016-04-29 17:00:00 +0300

    no message

--- services/src/main/java/by/grodno/ss/rentacar/service/BrandService.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/BrandService.java
@@ -1,5 +1,19 @@
 package by.grodno.ss.rentacar.service;
 
+import javax.transaction.Transactional;
+import by.grodno.ss.rentacar.datamodel.Brand;
+
+
 public interface BrandService {
 
+	Brand get(Long id);
+
+	@Transactional
+	void update(Brand brand);
+	
+	@Transactional
+	void set(Brand brand);
+	
+	@Transactional
+	void delete(Long id);
 }
