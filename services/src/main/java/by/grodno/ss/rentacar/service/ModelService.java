21c9f82a89a6deea2ef8db95a4524cb5a893d44e
serg <sstbox@gmail.com>
2016-05-03 16:52:35 +0300

    no message

--- services/src/main/java/by/grodno/ss/rentacar/service/ModelService.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/ModelService.java
@@ -1,5 +1,20 @@
 package by.grodno.ss.rentacar.service;
 
-public interface ModelService {
+import javax.transaction.Transactional;
 
+import by.grodno.ss.rentacar.datamodel.Brand;
+import by.grodno.ss.rentacar.datamodel.Model;
+
+public interface ModelService {
+	
+	Model get(Long id);
+
+	@Transactional
+	void update(Model model);
+	
+	@Transactional
+	void set(Model model);
+	
+	@Transactional
+	void delete(Long id);
 }
--- services/src/main/java/by/grodno/ss/rentacar/service/impl/ModelServiceImpl.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/impl/ModelServiceImpl.java
@@ -1,7 +1,49 @@
 package by.grodno.ss.rentacar.service.impl;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import by.grodno.ss.rentacar.dataaccess.BrandDao;
+import by.grodno.ss.rentacar.dataaccess.ModelDao;
+import by.grodno.ss.rentacar.datamodel.Model;
 import by.grodno.ss.rentacar.service.ModelService;
 
+@Service
 public class ModelServiceImpl implements ModelService {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+	
+	@Inject
+	private ModelDao modelDao;
+	
+	@Inject
+	private BrandDao brandDao;
+	
+	@Override
+	public Model get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Model model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(Model model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
