2b4bff8facc8205e28a28c6dba804732808927d7
serg <sstbox@gmail.com>
2016-05-03 12:07:05 +0300

    jpa modelgen

--- services/src/main/java/by/grodno/ss/rentacar/service/impl/BrandServiceImpl.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/impl/BrandServiceImpl.java
@@ -1,7 +1,42 @@
 package by.grodno.ss.rentacar.service.impl;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import by.grodno.ss.rentacar.dataaccess.BrandDao;
+import by.grodno.ss.rentacar.datamodel.Brand;
 import by.grodno.ss.rentacar.service.BrandService;
 
+@Service
 public class BrandServiceImpl implements BrandService {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+	
+	@Inject
+	private BrandDao brandDao;
+	
+	@Override
+	public Brand get(Long id) {
+		return brandDao.get(id);
+	}
+
+	@Override
+	public void update(Brand brand) {
+		brandDao.update(brand);
+	}
+
+	@Override
+	public void set(Brand brand) {
+		brandDao.insert(brand);
+	}
+
+	@Override
+	public void delete(Long id) {
+		LOGGER.info("Brand deleted: {}", brandDao.get(id).getBrandName());
+		brandDao.delete(id);
+	}
 
 }
---
+++ services/src/test/java/by/grodno/ss/rentacar/service/BrandServiceTest.java
@@ -0,0 +1,36 @@
+package by.grodno.ss.rentacar.service;
+
+import javax.inject.Inject;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import by.grodno.ss.rentacar.datamodel.Brand;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:service-context-test.xml" })
+public class BrandServiceTest {
+	
+	@Inject
+	private BrandService brandService;
+	
+	@Test
+	public void test(){
+		Brand brand = new Brand();
+		brand.setBrandName("Mitsubishi");
+		brandService.set(brand);
+		Brand brandMitsubishi = brandService.get(brand.getId());
+		Assert.assertNotNull(brandMitsubishi);
+		
+		String updatedBrandName = "Mazda";
+		brandMitsubishi.setBrandName(updatedBrandName);
+		brandService.update(brandMitsubishi);
+		Assert.assertEquals(updatedBrandName, brandService.get(brandMitsubishi.getId()).getBrandName());
+		
+		brandService.delete(brandMitsubishi.getId());
+        Assert.assertNull(brandService.get(brandMitsubishi.getId()));
+	}
+}
