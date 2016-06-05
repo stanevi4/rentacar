464e2cc0774f4b604ad8147025a8501da3665554
siarhei <sstbox@gmail.com>
2016-06-05 18:10:46 +0300

    admin page finish nice!

--- dataaccess/src/main/java/by/grodno/ss/rentacar/dataaccess/DamageDao.java
+++
@@ -1,7 +0,0 @@
-package by.grodno.ss.rentacar.dataaccess;
-
-import by.grodno.ss.rentacar.datamodel.Damage;
-
-public interface DamageDao extends AbstractDao<Damage, Long> {
-
-}
--- dataaccess/src/main/java/by/grodno/ss/rentacar/dataaccess/ModelDao.java
+++
@@ -1,7 +0,0 @@
-package by.grodno.ss.rentacar.dataaccess;
-
-import by.grodno.ss.rentacar.datamodel.Model;
-
-public interface ModelDao extends AbstractDao<Model, Long> {
-
-}
