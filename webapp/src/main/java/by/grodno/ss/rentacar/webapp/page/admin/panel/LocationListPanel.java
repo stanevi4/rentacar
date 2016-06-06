d0b838c21eb3c0c592731465b12bc68133926779
serg <sstbox@gmail.com>
2016-06-06 17:01:06 +0300

    no message

--- webapp/src/main/java/by/grodno/ss/rentacar/webapp/page/admin/panel/LocationListPanel.java
+++ webapp/src/main/java/by/grodno/ss/rentacar/webapp/page/admin/panel/LocationListPanel.java
@@ -1,22 +1,21 @@
 package by.grodno.ss.rentacar.webapp.page.admin.panel;
 
+import javax.inject.Inject;
+
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
+
+import by.grodno.ss.rentacar.service.LocationService;
 
 public class LocationListPanel extends Panel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@Inject
+	private LocationService locationService;
+	
 	public LocationListPanel(String id) {
 		super(id);
-		// TODO Auto-generated constructor stub
+		
 	}
-
-	public LocationListPanel(String id, IModel<?> model) {
-		super(id, model);
-		// TODO Auto-generated constructor stub
-	}
-
 }
