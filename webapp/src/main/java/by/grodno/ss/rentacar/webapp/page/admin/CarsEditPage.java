8bd35228ee82b0883363ee385d8d73362cba02f8
serg <sstbox@gmail.com>
2016-06-08 13:22:36 +0300

    no message

--- webapp/src/main/java/by/grodno/ss/rentacar/webapp/page/admin/CarsEditPage.java
+++ webapp/src/main/java/by/grodno/ss/rentacar/webapp/page/admin/CarsEditPage.java
@@ -1,20 +1,30 @@
 package by.grodno.ss.rentacar.webapp.page.admin;
 
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import by.grodno.ss.rentacar.webapp.page.admin.panel.CarListPanel;
+
 public class CarsEditPage extends AdminPage {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public CarsEditPage() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public CarsEditPage(PageParameters parameters) {
 		super(parameters);
-		// TODO Auto-generated constructor stub
 	}
-	/**
-	 * 
-	 */
+
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+
+		add(new FeedbackPanel("feedback"));
+		add(new CarListPanel("car-panel"));
+	}
 
 }
---
+++ webapp/src/main/java/by/grodno/ss/rentacar/webapp/page/admin/panel/CarListPanel.java
@@ -0,0 +1,20 @@
+package by.grodno.ss.rentacar.webapp.page.admin.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+public class CarListPanel extends Panel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CarListPanel(String id) {
+		super(id);
+	}
+
+	public CarListPanel(String id, IModel<?> model) {
+		super(id, model);
+	}
+
+}
