53250ef82b86f6dcac5058fe0d53629fe0914b73
serg <sstbox@gmail.com>
2016-06-09 17:05:28 +0300

    no message

--- services/src/main/java/by/grodno/ss/rentacar/service/OrderService.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/OrderService.java
@@ -1,5 +1,21 @@
 package by.grodno.ss.rentacar.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import by.grodno.ss.rentacar.dataaccess.filters.OrderFilter;
+import by.grodno.ss.rentacar.datamodel.Order;
+
 public interface OrderService {
 
+	Long count(OrderFilter filter);
+
+    List<Order> find(OrderFilter filter);
+
+    @Transactional
+    void saveOrUpdate(Order car);
+
+    @Transactional
+    void delete(Order car);
 }
--- services/src/main/java/by/grodno/ss/rentacar/service/impl/OrderServiceImpl.java
+++ services/src/main/java/by/grodno/ss/rentacar/service/impl/OrderServiceImpl.java
@@ -1,10 +1,38 @@
 package by.grodno.ss.rentacar.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import by.grodno.ss.rentacar.dataaccess.filters.OrderFilter;
+import by.grodno.ss.rentacar.datamodel.Order;
 import by.grodno.ss.rentacar.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public Long count(OrderFilter filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> find(OrderFilter filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveOrUpdate(Order car) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Order car) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
